f1219b9fb412c92ebf53d5c4ae74ecf0e7bf0009
Jelle van der Werff <jellevanderwerff@gmail.com>
2024-03-06 16:47:40 +0100

    reorganized behavioural plots

--- scripts/plots/fixel.py
+++ scripts/plots/fixel.py
@@ -2,55 +2,25 @@ import pandas as pd
 import os
 import scipy.io
 import numpy as np
-from nilearn import plotting
+from nilearn import plotting, datasets, image
 import matplotlib.pyplot as plt
-from matplotlib.colors import LinearSegmentedColormap
+
 
 # Load coordinates
-subcortical_dk = scipy.io.loadmat(os.path.join("data", "brain", "mappings", "dk_subcortical_84.mat"))
-node_names = subcortical_dk['namesNodes']
-
+subcortical_dk = pd.read_csv(os.path.join("data", "brain", "mappings", "cortical_subcortical_coords.csv"))
+coords = list(zip(subcortical_dk['X'], subcortical_dk['Y'], subcortical_dk['Z']))
+labels = subcortical_dk['Label']
 
 # Load data
-data = scipy.io.loadmat(os.path.join("data", "brain", "fixel", "cortical_thalamal_striatal_network.mat"))
-data = data[list(data.keys())[-1]]
+data = scipy.io.loadmat(os.path.join("data", "brain", "fixel", "cortical_thalamus_striatum_network.mat"))['connectivityMatrix']
 
-
-
-
-"""
-# Get adjacency matrix
-adj = data[list(data.keys())[-1]]
-# Change diagonal to zeros
-np.fill_diagonal(adj, 0)
-# Get indices of columns that are not all zeros
-indices = np.where(~np.all(adj == 0, axis=0))[0]
-# Remove rows and columns that are all zeros
-adj = adj[indices][:, indices]
-# Remove coordinates that are not in the adjacency matrix
-current_coords = [current_coords[i] for i in indices]
-# node_sizes
-node_sizes = np.sum(np.abs(adj), axis=1)
-# normalize node_sizes
-node_sizes = (node_sizes - np.min(node_sizes)) / (np.max(node_sizes) - np.min(node_sizes)) * 250
+# Get indices of rows that are not all zeros
+non_zero_rows = np.where(np.any(data, axis=1))[0]
+data = data[non_zero_rows, :]
+data = data[:, non_zero_rows]
+coords = [coords[i] for i in non_zero_rows]
+labels = [labels[i] for i in non_zero_rows]
 
 fig, ax = plt.subplots(figsize=(4, 4))
-
-# Plot connectome
-plotting.plot_connectome(
-    adj,
-    current_coords,
-    colorbar=True,
-    node_color="black",
-    edge_cmap='viridis' if not 'struct' in file else 'cividis',
-    edge_vmin=None if not 'struct' in file else 0,
-    edge_vmax=None if not 'struct' in file else np.max(adj),
-    node_size=node_sizes,
-    axes=ax,
-    display_mode="z",
-)
-
-# Save
-fig.savefig(os.path.join('plots', 'connectomes', f'{file[:-4]}_withcolorbar.pdf'))
-fig.savefig(os.path.join('plots', 'connectomes', f'{file[:-4]}_withcolorbar.png'), dpi=600)
-"""
+plotting.plot_connectome(data, coords, edge_vmin=0, edge_vmax=1, node_color='black', node_size=100, display_mode='y', edge_cmap=None, axes=ax)
+fig.savefig(os.path.join("plots", "fixel", "cortical_thalamus_striatum_network.pdf"))
